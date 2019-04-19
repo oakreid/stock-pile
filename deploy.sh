379107ace365427cc2f9adc8be6f9d322f54bcc5
Oakley Reid <oakreid@gmail.com>
2019-04-19 14:25:03 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+export MIX_ENV=prod
+export PORT=4795
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+
+echo "Building..."
+
+mkdir -p ~/.config
+mkdir -p priv/static
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix phx.digest
+mix ecto.create
+mix ecto.migrate
+
+echo "Generating release..."
+mix release.init
+mix release
+
+echo "Starting app..."
+
+_build/prod/rel/stock_pile/bin/stock_pile start
---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4795
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/stock_pile/bin/stock_pile stop || true
+
+echo "Starting app..."
+
+_build/prod/rel/stock_pile/bin/stock_pile foreground
